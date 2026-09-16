8302266b577bfb985fb541b3a7aaa98ddd0b8ed0
Asharulislam <immaster35@gmail.com>
2026-09-17 01:26:48 +0500

    ..

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,6 @@ import gymRoutes from './modules/gym/gym.routes'
 const app = express()
 app.use(express.json())
 
-app.get('/health', (req, res) => {
-  res.json({ status: 'ok' })
-})
 
 app.use('/auth', authRoutes)
 
