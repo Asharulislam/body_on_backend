bb98a0742c593a1810bf7eb708c3d458d1917683
Asharulislam <immaster35@gmail.com>
2026-09-21 22:05:05 +0500

    bug fixes

--- src/core/config/s3.ts
+++ src/core/config/s3.ts
@@ -1,16 +1,14 @@
 import { S3Client } from '@aws-sdk/client-s3'
 
 const region = process.env.AWS_REGION
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+const bucket = process.env.S3_BUCKET_NAME
 
-if (!region || !accessKeyId || !secretAccessKey) {
+if (!region || !bucket) {
   throw new Error('AWS S3 env variables are not set')
 }
 
 export const s3 = new S3Client({
   region,
-  credentials: { accessKeyId, secretAccessKey },
 })
 
-export const S3_BUCKET = process.env.S3_BUCKET!
+export const S3_BUCKET = bucket
